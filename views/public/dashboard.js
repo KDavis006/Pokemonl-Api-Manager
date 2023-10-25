7859b987ece6631c919fde56af7371c9bcfb068b
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-10-25 15:59:32 -0700

    trying to find an api

--- views/public/dashboard.js
+++ views/public/dashboard.js
@@ -1,12 +1,8 @@
-// import TCGdex from '@tcgdex/sdk';
-
-// const tcgdex = new TCGdex();
 
 const getAllCards = async() => {
- await axios.get('https://api.tcgdex.net/v2/en/cards')
- .then((res) => {
-  console.log(res)
- })
+ const allCards = await fetch('https://api.tcgdex.net/v2/en/cards')
+ .then(response => response.json())
+ console.log(allCards)
 }
 
 getAllCards();
